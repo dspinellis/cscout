a902ae3c94117d1215f145a4ff8202e3b57ff08f
Diomidis Spinellis <dds@aueb.gr>
2001-08-18 15:39:13 +0000

    Added accessor method for code.

--- refactor/token.h
+++ refactor/token.h
@@ -8,20 +8,23 @@
  * #include <deque>
  * #include "tokid.h"
  *
- * $Id: token.h,v 1.1 2001/08/18 15:29:45 dds Exp $
+ * $Id: token.h,v 1.2 2001/08/18 15:39:13 dds Exp $
  */
 
 #ifndef TOKEN_
 #define TOKEN_
 
 class Token {
-private:
-	int code;			// Token type code
 protected:
+	int code;			// Token type code
 	dequeTokid parts;		// Identifiers for constituent parts
 public:
 	Token(int icode) : code(icode) {};
+	// Accessor method
+	inline int get_code() const { return (code); }
+	// Return the token's symbolic name based on its code
 	string name() const;
+	// Send it on ostream
 	friend ostream& operator<<(ostream& o,const Token &t);
 };
 
