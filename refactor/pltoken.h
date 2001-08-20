4e9f6b519ba665962ad0f3e910e87ec15ed312f6
Diomidis Spinellis <dds@aueb.gr>
2001-08-20 15:37:26 +0000

    Converted more tokens to new interface.

--- refactor/pltoken.h
+++ refactor/pltoken.h
@@ -17,11 +17,12 @@
  * #include "fileid.h"
  * #include "tokid.h"
  * #include "token.h"
+ * #include "error.h"
  * #include "ptoken.h"
  * #include "fchar.h"
  * #include "ytab.h"
  *
- * $Id: pltoken.h,v 1.5 2001/08/20 08:43:07 dds Exp $
+ * $Id: pltoken.h,v 1.6 2001/08/20 15:37:26 dds Exp $
  */
 
 #ifndef PLTOKEN_
@@ -29,10 +30,15 @@
 
 class Fchar;
 
+// C-preprocessor context influences lexical analysis
+enum e_cpp_context {cpp_normal, cpp_include, cpp_define};
+
 class Pltoken: public Ptoken {
+private:
+	static enum e_cpp_context context;
 public:
-	Pltoken() {};
 	template <class C> void getnext();
+	static void set_context(enum e_cpp_context c) { context = c; };
 };
 
 /*
@@ -166,7 +172,6 @@ again:
 			val = (char)(code = '#');
 		}
 		break;
-#ifdef ndef
 	/* Operators starting with < or > */
 	case '>':
 		c0.getnext();
@@ -176,9 +181,9 @@ again:
 			val = ">=";
 			break;
 		case '>':
-			cn = in_get();
-			if (cn->get_char() == '=') {	/* >>= */
-				code = RIGHT_ASSIGN, c0, cn);
+			c0.getnext();
+			if (c0.get_char() == '=') {	/* >>= */
+				code = RIGHT_ASSIGN;
 				val = ">>=";
 			} else {			/* >> */
 				C::putback(c0);
@@ -193,113 +198,92 @@ again:
 		}
 		break;
 	case '<':
-		if (incpp) {
+		if (context == cpp_include) {
 			// C preprocessor #include <filename>
-			cn = NULL;
-			cn1 = c0;
-			do {
-				if (cn && cn != c0)
-					delete (cn);
-				cn = cn1;
-				cn1 = in_get();
-			} while (cn1->get_char() != EOF && cn1->get_char() != '>');
-			t->set_props(PATHFNAME, c0, cn1);
-			delete(c0);
-			if (cn != c0)
-				delete(cn);
-			if (cn1 != c0)
-				delete(cn1);
+			val = "";
+			for (;;) {
+				c0.getnext();
+				if (c0.get_char() == EOF || c0.get_char() == '>')
+					break;
+				val += c0.get_char();
+			}
+			code = PATHFNAME;
 			break;
 		}
-		cn1 = in_get();
-		switch (cn1->get_char()) {
+		c0.getnext();
+		switch (c0.get_char()) {
 		case '=':				/* <= */
-			t->set_props(LE_OP, c0, cn1); 
-			delete (cn1);
+			code = LE_OP; 
+			val = "<=";
 			break;
 		case '<':
-			cn = in_get();
-			if (cn->get_char() == '=') {	/* <<= */
-				t->set_props(LEFT_ASSIGN, c0, cn);
-				delete (cn);
+			c0.getnext();
+			if (c0.get_char() == '=') {	/* <<= */
+				code = LEFT_ASSIGN;
+				val = "<<=";
 			} else {			/* << */
-				in_unget(cn);
-				t->set_props(LEFT_OP, c0, cn1);
+				code = LEFT_OP;
+				val = "<<";
 			}
-			delete (cn1);
 			break;
 		default:				/* < */
-			in_unget(cn1);
-			t->set_props('<', c0, c0);
+			C::putback(c0);
+			val = (char)(code = '<');
 			break;
 		}
-		delete (c0);
 		break;
 	/* Comments and / operators */
 	case '/':
-		cn1 = in_get();
-		switch (cn1->get_char()) {
+		c0.getnext();
+		switch (c0.get_char()) {
 		case '=':				/* /= */
-			t->set_props(DIV_ASSIGN, c0, cn1); 
-			delete (c0);
-			delete (cn1);
+			code = DIV_ASSIGN; 
+			val = "/=";
 			break;
 		case '*':				/* Block comment */
-			delete (cn1);
-			cn1 = in_get();
+			c0.getnext();
 			for (;;) {
-				while (cn1->get_char() != '*' && cn1->get_char() != EOF) {
-					delete (cn1);
-					cn1 = in_get();
+				while (c0.get_char() != '*' && c0.get_char() != EOF) {
+					c0.getnext();
 				}
-				delete (cn1);
-				cn1 = in_get();
-				if (cn1->get_char() == EOF)
-					error(E_FATAL, c0, "EOF in comment");
-				if (cn1->get_char() == '/')
+				c0.getnext();
+				if (c0.get_char() == EOF)
+					Error::error(E_FATAL, "EOF in comment");
+				if (c0.get_char() == '/')
 					break;
 			}
-			t->set_props(SPACE, c0, cn1);
-			delete (c0);
-			delete (cn1);
+			code = SPACE;
+			val = " ";
 			break;
 		case '/':				/* Line comment */
-			delete (cn1);
-			cn = NULL;
-			cn1 = c0;
 			do {
-				if (cn && cn != c0)
-					delete (cn);
-				cn = cn1;
-				cn1 = in_get();
-			} while (cn1->get_char() != '\n' && cn1->get_char() != EOF);
-			in_unget(cn1);
-			t->set_props(SPACE, c0, cn);
-			delete (c0);
-			if (cn != c0)
-				delete (cn);
+				c0.getnext();
+			} while (c0.get_char() != '\n' && c0.get_char() != EOF);
+			C::putback(c0);
+			code = SPACE;
+			val = " ";
 			break;
 		default:				/* / */
-			in_unget(cn1);
-			t->set_props('/', c0, c0);
-			delete (c0);
+			C::putback(c0);
+			val = (char)(code = '/');
 			break;
 		}
 		break;
+#ifdef ndef
 	case '.':	/* . and ... */
 		cn1 = in_get();
-		if (isdigit(cn1->get_char())) {
+		if (isdigit(cn1.get_char())) {
 			cn = NULL;
 			goto float_fraction;
 		}
-		if (cn1->get_char() != '.') {
+		if (cn1.get_char() != '.') {
 			in_unget(cn1);
 			t->set_props('.', c0, c0);
 			delete (c0);
 			break;
 		}
 		cn = in_get();
-		if (cn->get_char() != '.') {
+		if (cn.get_char() != '.') {
 			in_unget(cn);
 			in_unget(cn1);
 			t->set_props('.', c0, c0);
@@ -324,22 +308,18 @@ again:
 		val = " ";
 		code = SPACE;
 		break;
-#ifdef ndef
 	/* Could be a long character or string */
 	case 'L':
-		cn1 = in_get();
-		switch (cn1->get_char()) {
+		c0.getnext();
+		switch (c0.get_char()) {
 		case '\'':
-			delete(cn1);
 			goto char_literal;
 		case '"':
-			delete(cn1);
 			goto string_literal;
 		default:
-			in_unget(cn1);
+			C::putback(c0);
 			goto identifier;
 		}
-#endif //ndef
 	case '_': case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': 
 	case 'g': case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': 
 	case 'n': case 'o': case 'p': case 'q': case 'r': case 's': case 't': 
@@ -349,6 +329,7 @@ again:
 	case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U': case 'V': 
 	case 'W': case 'X': case 'Y': case 'Z': 
 	identifier:
+		{
 		val = c0.get_char();
 		Tokid base = c0.get_tokid();
 		Tokid follow = base;
@@ -372,101 +353,73 @@ again:
 		copy(new_tokids.begin(), new_tokids.end(), back_inserter(parts));
 		// Later it will become TYPE_NAME, IDENTIFIER, or reserved word
 		code = IDENTIFIER;
+		}
 		break;
-#ifdef ndef
 	case '\'':
 	char_literal:
-		cn = NULL;
-		cn1 = c0;
 		n = 0;
-		do {
-			if (cn && cn != c0)
-				delete (cn);
-			cn = cn1;
-			cn1 = in_get();
-			if (cn1->get_char() == '\\') {
+		for (;;) {
+			c0.getnext();
+			if (c0.get_char() == '\\') {
 				// Consume one character after the backslash
 				// ... to deal with the '\'' problem
-				if (cn && cn != c0)
-					delete (cn);
-				cn = cn1;
-				cn1 = in_get();
+				val += '\\';
+				c0.getnext();
+				val += c0.get_char();
 				// And let the loop decide with the next one
-				if (cn && cn != c0)
-					delete (cn);
-				cn = cn1;
-				cn1 = in_get();
+				c0.getnext();
 				// We will deal with escapes later
 				n++;
 			}
+			if (c0.get_char() == EOF || c0.get_char() == '\'')
+				break;
+			val += c0.get_char();
 			n++;
-		} while (cn1->get_char() != EOF && cn1->get_char() != '\'');
-		t->set_props(INT_CONST, c0, cn1);
-		if (n == 1)
-			error(E_WARN, c0, "Empty character literal");
-		if (cn1->get_char() == EOF)
-			error(E_ERR, c0, "End of file in character literal");
-		delete(c0);
-		if (cn != c0)
-			delete(cn);
-		if (cn1 != c0)
-			delete(cn1);
+		}
+		val = CHAR_LIT;
+		if (n == 0)
+			Error::error(E_WARN, "Empty character literal");
+		if (c0.get_char() == EOF)
+			Error::error(E_ERR, "End of file in character literal");
 		break;
 	case '"':
 	string_literal:
-		if (incpp) {
+		if (context == cpp_include) {
 			// C preprocessor #include "filename"
-			cn = NULL;
-			cn1 = c0;
-			do {
-				if (cn && cn != c0)
-					delete (cn);
-				cn = cn1;
-				cn1 = in_get();
-			} while (cn1->get_char() != EOF && cn1->get_char() != '"');
-			t->set_props(ABSFNAME, c0, cn1);
-			delete(c0);
-			if (cn != c0)
-				delete(cn);
-			if (cn1 != c0)
-				delete(cn1);
+			for (;;) {
+				c0.getnext();
+				if (c0.get_char() == EOF || c0.get_char() == '"')
+					break;
+				val += c0.get_char();
+			}
+			code = ABSFNAME;
 			break;
 		}
-		cn = NULL;
-		cn1 = c0;
-		do {
-			if (cn && cn != c0)
-				delete (cn);
-			cn = cn1;
-			cn1 = in_get();
-			if (cn1->get_char() == '\\') {
+		for (;;) {
+			c0.getnext();
+			if (c0.get_char() == '\\') {
+				val += '\\';
 				// Consume one character after the backslash
-				if (cn && cn != c0)
-					delete (cn);
-				cn = cn1;
-				cn1 = in_get();
+				c0.getnext();
+				val += c0.get_char();
 				// And let the loop decide with the next one
-				if (cn && cn != c0)
-					delete (cn);
-				cn = cn1;
-				cn1 = in_get();
+				c0.getnext();
 				// We will deal with escapes later
 			}
-		} while (cn1->get_char() != EOF && cn1->get_char() != '"');
-		t->set_props(STRING_LITERAL, c0, cn1);
-		if (cn1->get_char() == EOF)
-			error(E_ERR, c0, "End of file in string literal");
-		delete(c0);
-		if (cn != c0)
-			delete(cn);
-		if (cn1 != c0)
-			delete(cn1);
+			if (c0.get_char() == EOF || c0.get_char() == '"')
+				break;
+			val += c0.get_char();
+		}
+		code = STRING_LITERAL;
+		if (c0.get_char() == EOF)
+			Error::error(E_ERR, "End of file in string literal");
 		break;
+#ifdef ndef
 	/* Various numbers */
 	case '0': case '1': case '2': case '3': case '4':
 	case '5': case '6': case '7': case '8': case '9': 
 		cn1 = in_get();
-		if (cn1->get_char() == 'x' || cn1->get_char() == 'X') {
+		if (cn1.get_char() == 'x' || cn1.get_char() == 'X') {
 			// Hex constant
 			cn = NULL;
 			n = 0;
@@ -476,19 +429,19 @@ again:
 				cn = cn1;
 				cn1 = in_get();
 				n++;
-			} while (isxdigit(cn1->get_char()));
+			} while (isxdigit(cn1.get_char()));
 			if (n == 1)
-				error(E_ERR, c0, "Missing digit in hex constant");
+				Error::error(E_ERR, c0, "Missing digit in hex constant");
 		} else {
 			// Decimal constant
 			cn = NULL;
-			while (isdigit(cn1->get_char())) {
+			while (isdigit(cn1.get_char())) {
 				if (cn)
 					delete (cn);
 				cn = cn1;
 				cn1 = in_get();
 			}
-			switch (cn1->get_char()) {
+			switch (cn1.get_char()) {
 			case 'e': case 'E':
 				goto float_exp;
 			case '.':
@@ -503,7 +456,7 @@ again:
 		bool have_u, have_l;
 		have_u = have_l = false;
 		for (;;) {
-			switch (cn1->get_char()) {
+			switch (cn1.get_char()) {
 			case 'u': case 'U':
 				if (have_u)
 					error(E_ERR, c0, "Bad integer suffix");
@@ -538,25 +491,25 @@ again:
 		 * digits before the ., so we know the number has at least
 		 * one digit.
 		 */
-		 while (isdigit(cn1->get_char())) {
+		 while (isdigit(cn1.get_char())) {
 			if (cn)
 				delete (cn);
 			cn = cn1;
 			cn1 = in_get();
 		}
-		if (cn1->get_char() == 'e' || cn1->get_char() == 'E') {
+		if (cn1.get_char() == 'e' || cn1.get_char() == 'E') {
 	float_exp:
 			if (cn)
 				delete(cn);
 			cn = cn1;
 			cn1 = in_get();
-			if (cn1->get_char() == '+' || cn1->get_char() == '-') {
+			if (cn1.get_char() == '+' || cn1.get_char() == '-') {
 				delete(cn);
 				cn = cn1;
 				cn1 = in_get();
 			}
 			n = 0;
-			while (isdigit(cn1->get_char())) {
+			while (isdigit(cn1.get_char())) {
 				delete (cn);
 				cn = cn1;
 				cn1 = in_get();
@@ -566,7 +519,7 @@ again:
 				error(E_ERR, c0, "Malformed floating point exponent");
 		}
 		// Floating point suffix (in cn1)
-		switch (cn1->get_char()) {
+		switch (cn1.get_char()) {
 		case 'f': case 'F': case 'l': case 'L':
 			if (cn)
 				delete(cn);
@@ -581,9 +534,10 @@ again:
 		break;
 	default:
 		error(E_WARN, c0, "Bad character value %c (%d - 0x%02x)", 
-			c0->get_char(), c0->get_char(), c0->get_char());
+			c0.get_char(), c0.get_char(), c0.get_char());
 		goto again;
 #endif  //ndef
 	}
 }
+
 #endif // PLTOKEN
