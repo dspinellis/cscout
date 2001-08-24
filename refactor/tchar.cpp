96086de5efb1ca6e56257a097e51fec53f436248
Diomidis Spinellis <dds@aueb.gr>
2001-08-24 10:54:29 +0000

    Initial revision

---
+++ refactor/tchar.cpp
@@ -0,0 +1,74 @@
+/* 
+ * (C) Copyright 2001 Diomidis Spinellis.
+ *
+ * For documentation read the corresponding .h file
+ *
+ * $Id: tchar.cpp,v 1.1 2001/08/24 10:54:29 dds Exp $
+ */
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <deque>
+#include <cassert>
+#include <fstream>
+#include <stack>
+
+#include "cpp.h"
+#include "fileid.h"
+#include "tokid.h"
+#include "token.h"
+#include "ptoken.h"
+#include "fchar.h"
+#include "tchar.h"
+
+stackTchar Tchar::ps;			// Putback Tchars (from putback())
+
+void
+Tchar::putback(Tchar c)
+{
+	ps.push(c);
+}
+
+
+void
+Tchar::getnext()
+{
+	if (!ps.empty()) {
+		*this = ps.top();
+		ps.pop();
+		return;
+	}
+	if (qi == iq.end()) {
+		val = EOF;
+		return;
+	}
+	const Ptoken& pt = (*qi);
+	const Tpart& tp = *pi;
+	ti = tp.get_tokid() + part_idx;
+	val = pt.get_val()[val_idx];
+	// Advance pointers
+	val_idx++;
+	if (++part_idx == tp.get_len()) {
+		if (++pi == pt.get_parts_end()) {
+			if (++qi == iq.end())
+				return;
+			pi = (*qi).get_parts_begin();
+		}
+		part_idx = 0;
+	}
+}
+
+void
+Tchar::rewind_input()
+{
+	qi = iq.begin();
+	pi = (*qi).get_parts_begin();
+	part_idx = val_idx = 0;
+}
+
+void
+Tchar::push_input(const Ptoken& p)
+{
+	iq.push_back(p);
+}
---
+++ refactor/tchar.h
@@ -0,0 +1,72 @@
+/* 
+ * (C) Copyright 2001 Diomidis Spinellis.
+ *
+ * A character coming from a token.
+ *
+ * Include synopsis:
+ * #include <fstream>
+ * #include <stack>
+ * #include <deque>
+ * #include <map>
+ *
+ * #include "fileid.h"
+ * #include "cpp.h"
+ * #include "tokid.h"
+ * #include "tchar.h"
+ *
+ * $Id: tchar.h,v 1.1 2001/08/24 10:54:29 dds Exp $
+ */
+
+#ifndef TCHAR_
+#define TCHAR_
+
+using namespace std;
+
+
+class Tchar;
+
+// For pushback; two token stack needed to parse the "..."
+typedef stack <Tchar> stackTchar;
+
+typedef deque <Ptoken> dequePtoken;
+
+class Tchar {
+private:
+	static stackTchar ps;		// Putback stack
+	static dequePtoken iq;		// Input queue
+	// The following always point to the next character to return
+	// If any points to end() it means there is nothing more to return
+	// Token from iq use for getnext
+	static dequePtoken::const_iterator qi;
+	// Token part from *ip to use for getnext
+	static dequeTpart::const_iterator pi;
+	// Index to character of token part to use for getnext
+	static int part_idx;
+	// Index to character from val to use for getnext
+	static int val_idx;
+
+	int val;
+	Tokid ti;			// (pos_type from tellg(), fi)
+
+public:
+	// Call it before the first getnext()
+	static void rewind_input();
+	// Will read characters from tokens p
+	static void push_input(const Ptoken& p);
+	// Getnext will return c
+	static void putback(Tchar c);
+
+	// Construct an unititialized one
+	Tchar() {};
+	// Construct it with a given value and tokid
+	Tchar(int v, Tokid t) : val(v), ti(t) {};
+	Tchar(int v) : val(v) {};
+	// Read next from stream
+	void getnext();
+	// Return the character value (or EOF)
+	inline int get_char() const { return (val); };
+	// Return the character's Tokid
+	inline Tokid get_tokid() const { return (ti); };
+};
+
+#endif /* TCHAR_ */
