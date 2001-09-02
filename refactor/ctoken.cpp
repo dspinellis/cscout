07ef6f3eabf58a1b9473044b8cc836209194f459
Diomidis Spinellis <dds@aueb.gr>
2001-09-02 12:43:27 +0000

    Can now handle hex and octal escape sequences.

--- refactor/ctoken.cpp
+++ refactor/ctoken.cpp
@@ -3,11 +3,13 @@
  *
  * For documentation read the corresponding .h file
  *
- * $Id: ctoken.cpp,v 1.1 2001/09/02 09:23:58 dds Exp $
+ * $Id: ctoken.cpp,v 1.2 2001/09/02 12:43:27 dds Exp $
  */
 
 #include <string>
+#include <algorithm>
 #include <cctype>
+#include <cstdlib>		// strtol
 
 #include "cpp.h"
 #include "error.h"
@@ -19,13 +21,15 @@
 char
 unescape_char(const string& s, string::const_iterator& si)
 {
+	int val;
+
 	if (*si != '\\')
 		return *si++;
 	if (++si == s.end()) {
 		Error::error(E_ERR, "Unexpected end in character escape sequence");
 		return 0;
 	}
-	switch (*si++) {
+	switch (val = *si++) {
 	case '\'': return '\'';
 	case '"': return '"';
 	case '?': return '?';
@@ -37,6 +41,34 @@ unescape_char(const string& s, string::const_iterator& si)
 	case 'r': return '\r';
 	case 't': return '\t';
 	case 'v': return '\v';
+
+	case '0': case '1': case '2': case '3':
+	case '4': case '5': case '6': case '7':
+		val -= '0';
+		// Up to three digits maximum; strtol is useless.
+		for (int i = 0; i < 2; i++) {
+			if (si == s.end() || *si < '0' || *si > '7')
+				break;
+			val *= 8;
+			val += *si++ - '0';
+		}
+		// if (si != s.end()) cout << "Next char is [" << *si << "]\n";
+		return (val);
+	case 'x': {
+		string tmp(si - 2, s.end());
+		char *endptr;
+		tmp[0] = '0';		// For strtoul 0xddd
+		// cout << "tmp=" << tmp << "\n";
+		const char *cs = tmp.c_str();
+		val = strtol(cs, &endptr, 0);
+		if (endptr - cs == 1)
+			// All we could read was 0x
+			Error::error(E_ERR, "Illegal characters in hex escape sequence");
+		si += endptr - cs - 2;
+		// if (si != s.end()) cout << "Next char is [" << *si << "]\n";
+		return (val);
+	}
+
 	default:
 		Error::error(E_ERR, "Invalid character escape sequence");
 		return 0;
