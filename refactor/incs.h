850b6db235dc01c4ccc2c233f6b916961779d5a3
Diomidis Spinellis <dds@aueb.gr>
2002-09-05 16:43:58 +0000

    Initial revision

---
+++ refactor/incs.h
@@ -0,0 +1,16 @@
+/* 
+ * (C) Copyright 2001 Diomidis Spinellis.
+ *
+ * Define include paths
+ *
+ * To create this file look at /usr/lib/gcc-lib/ * / * /specs
+ *
+ * $Id: incs.h,v 1.1 2002/09/05 16:43:58 dds Exp $
+ */
+
+#ifdef __cplusplus
+#pragma includepath "/usr/include/g++-3"
+#endif
+
+#pragma includepath "/usr/lib/gcc-lib/i386-redhat-linux/2.96/include"
+#pragma includepath "/usr/include"
