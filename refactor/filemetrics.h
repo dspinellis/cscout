74fe56a6c6c2b603390a5a965d5890b3d68d2e6f
Diomidis Spinellis <dds@aueb.gr>
2002-09-17 07:56:14 +0000

    Initial revision

---
+++ refactor/filemetrics.h
@@ -0,0 +1,105 @@
+/* 
+ * (C) Copyright 2002 Diomidis Spinellis.
+ *
+ * A metrics container.
+ *
+ * One metrics object is created and updated for every file.
+ * Most metrics are collected during the postprocessing phase to
+ * keep the main engine fast and clean.
+ * This design also ensures that the character-based metrics processing 
+ * overhead will be incured exactly once for each file.
+ *
+ * $Id: filemetrics.h,v 1.1 2002/09/17 07:56:14 dds Exp $
+ */
+
+#ifndef METRICS_
+#define METRICS_
+
+class Metrics {
+private:
+// Matrics we collect
+// During post-processing
+	int nchar;		// Number of characters
+	int nlcomment;		// Number of line comments
+	int nbcomment;		// Number of block comments
+	int nline;		// Number of lines
+	int maxlinelen;		// Maximum number of characters in a line
+	int nccomment;		// Comment characters
+	int nspace;		// Space characters
+
+// During processing (once based on processed)
+	int nfunction;		// Defined functions (function_brace_begin)
+	int nppdirective;	// Number of cpp directives
+	int nincfile;		// Number of directly included files
+	int nstatement;		// Number of statements
+
+// Helper variables
+	bool processed;		// True after file has been processed
+	int currlinelen;
+	// States while processing characters
+	enum e_state {
+		s_normal,
+		s_saw_slash,		// After a / character
+		s_cpp_comment,		// Inside C++ comment
+		s_block_comment,	// Inside C block comment
+		s_block_star,		// Found a * in a block comment
+	} cstate;
+
+public:
+	Metrics() :
+		nchar(0),
+		nlcomment(0),
+		nbcomment(0),
+		nline(0),
+		maxlinelen(0),
+		nccomment(0),
+		nspace(0),
+
+		nfunction(0),
+		nppdirective(0),
+		nincfile(0),
+		nstatement(0),
+
+		processed(false),
+		currlinelen(0),
+		cstate(s_normal)
+	{}
+
+	// Called for all file characters appart from identifiers
+	void process_char(char c);
+	// Called for every identifier
+	void process_id(const string &s);
+
+	// Manipulate the processing-based metrics
+	void add_ppdirective() { if (!processed) nppdirective++; }
+	void add_incfile() { if (!processed) nincfile++; }
+	void add_statement() { if (!processed) nstatement++; }
+	void add_function() { if (!processed) nfunction++; }
+	void done_processing() { processed = true; }
+	
+	// Get methods
+	// Number of characters
+	int get_nchar() const { return nchar; }		
+	// Number of line comments
+	int get_nlcomment() const { return nlcomment; }		
+	// Number of block comments
+	int get_nbcomment() const { return nbcomment; }		
+	// Number of lines
+	int get_nline() const { return nline; }		
+	// Maximum number of characters in a line
+	int get_maxlinelen() const { return maxlinelen; }		
+	// Comment characters
+	int get_nccomment() const { return nccomment; }		
+	// Space characters
+	int get_nspace() const { return nspace; }		
+	// Defined functions (function_brace_begin)
+	int get_nfunction() const { return nfunction; }		
+	// Number of cpp directives
+	int get_nppdirective() const { return nppdirective; }	
+	// Number of directly included files
+	int get_nincfile() const { return nincfile; }		
+	// Number of statements
+	int get_nstatement() const { return nstatement; }		
+};
+
+#endif /* METRICS_ */
---
+++ refactor/metrics.cpp
@@ -0,0 +1,90 @@
+/* 
+ * (C) Copyright 2002 Diomidis Spinellis.
+ *
+ * For documentation read the corresponding .h file
+ *
+ * $Id: metrics.cpp,v 1.1 2002/09/17 07:56:14 dds Exp $
+ */
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <deque>
+#include <cassert>
+#include <fstream>
+#include <stack>
+#include <set>
+#include <vector>
+#include <list>
+#include <errno.h>
+
+#include "cpp.h"
+#include "debug.h"
+#include "metrics.h"
+#include "fileid.h"
+#include "attr.h"
+#include "tokid.h"
+#include "fchar.h"
+#include "error.h"
+
+// Called for every identifier
+void 
+Metrics::process_id(const string &s)
+{
+	nchar += s.length();
+	currlinelen += s.length();
+}
+
+// Called for all file characters appart from identifiers
+void 
+Metrics::process_char(char c)
+{
+	nchar++;
+	if (c == '\n') {
+		if (DP())
+			cout << "nline = " << nline << "\n";
+		nline++;
+		if (currlinelen > maxlinelen)
+			maxlinelen = currlinelen;
+		currlinelen = 0;
+	} else
+		currlinelen++;
+	switch (cstate) {
+	case s_normal:
+		if (isspace(c))
+			nspace++;
+		else if (c == '/')
+			cstate = s_saw_slash;
+		break;
+	case s_saw_slash:		// After a / character
+		if (c == '/') {
+			cstate = s_cpp_comment;
+			nlcomment++;
+		} else if (c == '*') {
+			cstate = s_block_comment;
+			nbcomment++;
+		} else
+			cstate = s_normal;
+		break;
+	case s_cpp_comment:		// Inside C++ comment
+		if (c == '\n')
+			cstate = s_normal;
+		else
+			nccomment++;
+		break;
+	case s_block_comment:		// Inside C block comment
+		if (c == '*')
+			cstate = s_block_star;
+		nccomment++;
+		break;
+	case s_block_star:		// Found a * in a block comment
+		if (c == '/')
+			cstate = s_normal;
+		else
+			cstate = s_block_comment;
+		nccomment--;		// Don't count the trailing */
+		break;
+	default:
+		assert(0);
+	}
+}
