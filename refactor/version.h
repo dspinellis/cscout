c1b939a12fee3fc0ce292c2e37871e2b84bf1fd2
Diomidis Spinellis <dds@aueb.gr>
2003-06-04 04:39:58 +0000

    Added revision and date.

--- refactor/version.h
+++ refactor/version.h
@@ -1,15 +1,16 @@
 /* 
  * (C) Copyright 2003 Diomidis Spinellis.
  *
- * Access the RCS and global version strings
+ * Access the RCS and global version identifiers
  *
  * Include synopsis: version.h
  *
- * $Id: version.h,v 1.1 2003/06/03 08:40:52 dds Exp $
+ * $Id: version.h,v 1.2 2003/06/04 04:39:58 dds Exp $
  */
 
 class Version {
 public:
-	static char ident[];		// RCS Id for all source files
-	static char version[];		// Global version Id
+	static char revision[];		// Global revision number 
+	static char date[];		// Version release date
+	static char *ident[];		// RCS Id for all source files
 };
