b068ce1ed3a2bdd10d18dd926bc370d1b8aacb03
Diomidis Spinellis <dds@aueb.gr>
2003-07-07 15:37:29 +0000

    New version

--- refactor/version.cpp
+++ refactor/version.cpp
@@ -1,20 +1,20 @@
 #include <string>
 using namespace std;
 #include "version.h"
-char Version::revision[] = "$Revision: 1.12 $";
-char Version::date[] = "$Date: 2003/06/23 10:07:57 $";
+char Version::revision[] = "$Revision: 1.13 $";
+char Version::date[] = "$Date: 2003/07/07 15:37:29 $";
 char *Version::ident[] = {
-"$" "Id:" " Makefile 1.36 2003/06/16 20:22:40 dds Exp dds $",
+"$" "Id:" " Makefile 1.38 2003/07/07 13:37:09 dds Exp $",
 "$" "Id:" " attr.cpp 1.11 2003/06/16 06:22:29 dds Exp $",
 "$" "Id:" " attr.h 1.10 2003/05/27 19:57:35 dds Exp $",
 "$" "Id:" " color.cpp 1.9 2002/09/28 13:22:40 dds Exp $",
-"$" "Id:" " cscout.cpp 1.47 2003/06/23 09:18:44 dds Exp $",
-"$" "Id:" " ctoken.cpp 1.21  2003/06/23 09:17:42 dds Exp $",
+"$" "Id:" " cscout.cpp 1.49 2003/07/07 13:37:09 dds Exp $",
+"$" "Id:" " ctoken.cpp 1.21 20033/06/23 09:17:42 dds Exp $",
 "$" "Id:" " ctoken.h 1.4 2001/09/12 07:09:08 dds Exp $",
 "$" "Id:" " debug.cpp 1.2 2001/09/03 08:13:51 dds Exp $",
 "$" "Id:" " debug.h 1.4 2002/12/26 12:46:24 dds Exp $",
 "$" "Id:" " defs.h 1.12 2002/12/25 19:35:42 dds Exp $",
-"$" "Id:" " des.c 1.5 2003/05/17 10:30:52 dds Exp $",
+"$" "Id:" " des.c 1.6 2003/06/23 12:17:25 dds Exp $",
 "$" "Id:" " des.h 1.3 2003/05/17 07:38:32 dds Exp $",
 "$" "Id:" " eclass.cpp 1.23 2003/06/01 08:52:05 dds Exp $",
 "$" "Id:" " eclass.h 1.17 2003/05/27 19:57:35 dds Exp $",
@@ -29,15 +29,15 @@ char *Version::ident[] = {
 "$" "Id:" " id.h 1.2 2001/09/21 14:14:19 dds Exp $",
 "$" "Id:" " incs.h 1.1 2002/09/05 16:43:58 dds Exp $",
 "$" "Id:" " license.c 1.8 2003/06/22 23:27:04 dds Exp $",
-"$" "Id:" " license.h 1.4 2003/06/23 07:51:15 dds Exp $",
+"$" "Id:" " license.h 1.5 2003/07/07 12:32:42 dds Exp $",
 "$" "Id:" " macro.cpp 1.12 2003/06/01 09:03:06 dds Exp $",
 "$" "Id:" " macro.h 1.4 2002/12/15 19:03:37 dds Exp $",
 "$" "Id:" " metrics.cpp 1.9 2003/06/02 13:51:01 dds Exp $",
 "$" "Id:" " metrics.h 1.9 2003/06/22 23:27:04 dds Exp $",
 "$" "Id:" " defs.h 1.9 2002/09/05 16:51:23 dds Exp $",
 "$" "Id:" " incs.h 1.1 2002/09/05 16:43:58 dds Exp $",
-"$" "Id:" " obfuscate.cpp 1.1 2002/10/06 18:39:05 dds Exp $",
-"$" "Id:" " parse.y 1.57 2003/06/22 19:19:12 dds Exp $",
+"$" "Id:" " obfuscate.cpp 1.2 2003/07/02 10:47:15 dds Exp $",
+"$" "Id:" " parse.y 1.58 2003/07/05 10:17:17 dds Exp $",
 "$" "Id:" " pdtoken.cpp 1.74 2003/06/19 11:11:01 dds Exp $",
 "$" "Id:" " pdtoken.h 1.23 2002/12/15 19:03:37 dds Exp $",
 "$" "Id:" " pltoken.cpp 1.10 2003/06/01 08:54:27 dds Exp $",
@@ -68,4 +68,6 @@ char *Version::ident[] = {
 "$" "Id:" " ytoken.h 1.5 2003/06/19 11:11:01 dds Exp $",
 "$" "Id:" " prjcomp.pl 1.11 2003/06/22 23:33:34 dds Exp $",
 "$" "Id:" " version.h 1.3 2003/06/04 18:37:03 dds Exp $",
+"$" "Id:" " getopt.c 1.1 2003/07/07 13:36:54 dds Exp $",
+"$" "Id:" " getopt.h 1.1 2003/07/07 13:36:54 dds Exp $",
 0 };
