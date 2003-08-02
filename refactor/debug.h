62665358733efc0ef7a0ba92d4b334f2d3831216
Diomidis Spinellis <dds@aueb.gr>
2003-08-02 10:52:22 +0000

    More efficient string passing.

--- refactor/debug.h
+++ refactor/debug.h
@@ -5,7 +5,7 @@
  * The debugpoint() macro should be used to efficiently disable debugging output
  * A debugpoint on a file's 0 line will enable all debugpoints for that file
  *
- * $Id: debug.h,v 1.5 2003/08/02 09:35:22 dds Exp $
+ * $Id: debug.h,v 1.6 2003/08/02 10:52:22 dds Exp $
  */
 
 #ifndef DEBUG_
@@ -22,7 +22,7 @@ private:
 public:
 	Debug(const string f, int l) : fname(f), line(l) {};
 
-	static inline bool is_db_set(const string fname, int line);
+	static inline bool is_db_set(const string &fname, int line);
 	static void db_set(string fname, int line);
 	static void db_read();
 	static inline bool is_enabled() { return enabled; };
@@ -41,10 +41,10 @@ operator <(const class Debug a, const class Debug b)
 }
 
 inline bool
-Debug::is_db_set(const string fname, int line)
+Debug::is_db_set(const string &fname, int line)
 {
-	return dp.find(Debug(fname, line)) != dp.end() ||
-	       dp.find(Debug(fname, 0)) != dp.end();
+	return (dp.find(Debug(fname, line)) != dp.end()) ||
+	       (dp.find(Debug(fname, 0)) != dp.end());
 }
 
 #endif /* DEBUG_ */
