6d329222999fa42db234e8ce97a92c3af4ea1e14
Diomidis Spinellis <dds@aueb.gr>
2003-08-11 14:13:49 +0000

    Initial revision

---
+++ refactor/fdep.cpp
@@ -0,0 +1,45 @@
+/* 
+ * (C) Copyright 2001 Diomidis Spinellis.
+ *
+ * For documentation read the corresponding .h file
+ *
+ * $Id: fdep.cpp,v 1.1 2003/08/11 14:13:49 dds Exp $
+ */
+
+#include <set>
+#include <map>
+#include <string>
+#include <vector>
+#include <list>
+
+#include "cpp.h"
+#include "debug.h"
+#include "attr.h"
+#include "metrics.h"
+#include "fileid.h"
+#include "fdep.h"
+
+bool Fdep::monitoring = false;			// True if we are monitoring file dependencies
+map <Fileid, set <Fileid> > Fdep::definers;	// Files containing definitions needed in a given file
+set <Fileid> Fdep::providers;			// Files providing code and data
+set <Fileid> Fdep::direct_includes;		// Files included directly
+Fileid Fdep::last_provider;			// Cache last value entered
+
+/* 
+ * Mark as used:
+ * - the passed file (the file currently being processed)
+ * - all files that provided code and data
+ * - all the files that contain definitions for them 
+ */
+void
+Fdep::mark_required(Fileid f)
+{
+	if (f.required())
+		return;
+	f.set_required(true);
+	const set <Fileid> &defs = definers[f];
+	for (set <Fileid>::const_iterator i = defs.begin(); i != defs.end(); i++)
+		mark_required(*i);
+	for (set <Fileid>::const_iterator i = providers.begin(); i != providers.end(); i++)
+		mark_required(*i);
+}
---
+++ refactor/fdep.h
@@ -0,0 +1,73 @@
+/* 
+ * (C) Copyright 2003 Diomidis Spinellis.
+ *
+ * File dependency monitoring
+ *
+ * Include synopsis:
+ * #include <set>
+ * #include <map>
+ * #include <string>
+ * #include <vector>
+ * #include <list>
+ *
+ * #include "attr.h"
+ * #include "metrics.h"
+ * #include "fileid.h"
+ *
+ * $Id: fdep.h,v 1.1 2003/08/11 14:13:49 dds Exp $
+ */
+
+#ifndef FDEP_
+#define FDEP_
+
+
+// A container for file dependencies
+class Fdep {
+private:
+	static bool monitoring;				// True if we are monitoring file dependencies
+	static map <Fileid, set <Fileid> > definers;	// Files containing definitions needed in a given file
+	static set <Fileid> providers;			// Files providing code and data
+	static set <Fileid> direct_includes;		// Files included directly
+	static Fileid last_provider;			// Cache last value entered
+public:
+	static bool monitoring_dependencies() { return monitoring; }
+	static void set_monitoring_dependencies(bool v) { monitoring = v; }
+	// File def contains a definition needed by file ref
+	static void add_def_ref(Fileid def, Fileid ref) {
+		if (!monitoring || def == ref)
+			return;
+		definers[ref].insert(def);
+	}
+	static void add_provider(Fileid f) {
+		if (!monitoring || f == last_provider)
+			return;
+		providers.insert(f);
+		last_provider = f;
+	}
+	// Mark f as directly included (included from the processed file)
+	static void add_direct_include(Fileid f) {
+		if (!monitoring)
+			return;
+		direct_includes.insert(f);
+	}
+	// Return true if f is directly included
+	static bool is_directly_included(Fileid f) {
+		return direct_includes.find(f) != direct_includes.end();
+	}
+	/* 
+	 * Mark as used:
+	 * - the passed file (the file currently being processed)
+	 * - all files that provided code and data
+	 * - all the files that contain definitions for them 
+	 */
+	static void mark_required(Fileid f);
+	// Clear definers and providers starting another round
+	static void reset() {
+		definers.clear();
+		providers.clear();
+		last_provider = Fileid();	// Clear cache
+	}
+};
+
+
+#endif /* FDEP_ */
