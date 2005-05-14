c47cc2b86d5a841c8a30667859d159e75639e4bb
Diomidis Spinellis <dds@aueb.gr>
2005-05-14 13:18:34 +0000

    Initial revision

---
+++ refactor/workdb.h
@@ -0,0 +1,9 @@
+/*
+ * (C) Copyright 1995-2005 Diomidis Spinellis.
+ *
+ * Licensing client and server functions
+ *
+ * $Id: workdb.h,v 1.1 2005/05/14 13:18:34 dds Exp $
+ */
+
+int workdb(const char *);
