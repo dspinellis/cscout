7b8286e0f6fd251aa5316ddb1bf1e45b4ab9f89b
dspinellis <dspinellis>
2006-07-30 22:17:05 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.45.1'.

--- refactor/cpp.h
+++
@@ -1,9 +0,0 @@
-using namespace std;
-
-typedef long cs_offset_t;
-
-#ifdef NDEBUG
-#define ASSERT(x) ((void)0)
-#else
-#define ASSERT(x) (x ? (void)0 : (Error::error(E_INTERNAL, "Failed assertion"), assert(x)))
-#endif
