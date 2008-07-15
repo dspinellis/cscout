8be7fb5dfccf0ceffd62a7204183e0a5e40e656a
Diomidis Spinellis <dds@aueb.gr>
2008-07-15 15:51:25 +0000

    Add comment and RCS identifier.

--- refactor/dirbrowse.h
+++ refactor/dirbrowse.h
@@ -1,3 +1,11 @@
+/*
+ * (C) Copyright 2008 Diomidis Spinellis.
+ *
+ * Web directory browsing functions
+ *
+ * $Id: dirbrowse.h,v 1.2 2008/07/15 15:51:25 dds Exp $
+ */
+
 // Add a file to the directory tree for later browsing
 void *dir_add_file(Fileid f);
 // Display a directory's contents
--- refactor/html.h
+++ refactor/html.h
@@ -1,3 +1,11 @@
+/*
+ * (C) Copyright 2008 Diomidis Spinellis.
+ *
+ * HTML helper functions.
+ *
+ * $Id: html.h,v 1.2 2008/07/15 15:51:25 dds Exp $
+ */
+
 const char * html(char c);
 string html(const string &s);
 void html_string(FILE *of, string s);
