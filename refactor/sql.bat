088f643f873147520d1dd337e0ead645ba7a9679
dspinellis <dspinellis>
2008-12-05 11:11:53 +0000

    This commit was manufactured by cvs2svn to create tag 'R2-6'.

--- refactor/sql.bat
+++
@@ -1,4 +0,0 @@
-cd ..\example
-..\refactor\i386\cscout -s hsqldb awk.cs >\tmp\awk.sql
-cd ..\refactor
-java -classpath /app/hsqldb/lib/hsqldb.jar org.hsqldb.util.DatabaseManager -script \tmp\awk.sql
