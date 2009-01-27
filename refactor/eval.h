4fa85b7d2f1a666a69a0fb7a00d8972a1e8efef9
Diomidis Spinellis <dds@aueb.gr>
2009-01-27 10:22:10 +0000

    According to C99 6.10.1.3 during preprocessing expression evaluation all integer types act as if the have the same representation intmax_t/uintmax_t.

--- refactor/eval.h
+++ refactor/eval.h
@@ -9,19 +9,32 @@
  * between the lexical analyzer and the preprocessor expression
  * evaluation parser.
  *
- * $Id: eval.h,v 1.3 2009/01/15 14:32:57 dds Exp $
+ * $Id: eval.h,v 1.4 2009/01/27 10:22:10 dds Exp $
  *
  */
 
+
+#ifndef EVAL_H_INCLUDED
+#define EVAL_H_INCLUDED
+
+#include "stdint.h"
+
 enum e_su {e_signed, e_unsigned};		// Signed or unsigned
 
+/*
+ * According to C99 6.10.1.3 during preprocessing expression
+ * evaluation all integer types act as if the have the same
+ * representation intmax_t/uintmax_t.
+ */
 struct s_ppval {
 	union {
-		unsigned long u;	// Unsigned
-		signed long s;		// Signed
+		uintmax_t u;	// Unsigned
+		intmax_t s;	// Signed
 	} v;
-	enum e_su su;			// Signed or unsigned
+	enum e_su su;		// Signed or unsigned
 };
 
 extern struct s_ppval eval_lval;
 extern int eval_parse();
+
+#endif /* EVAL_H_INCLUDED */
