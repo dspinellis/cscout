6ae0f44c3b799211f0d60865ca33a0f81323a21b
Diomidis Spinellis <dds@aueb.gr>
2012-09-10 17:01:26 +0000

    Conform to new syntax Add tokens table

--- refactor/cscout-data.sql
+++ refactor/cscout-data.sql
@@ -30,9 +30,10 @@
 #include "metrics.h"
 #include "fileid.h"
 #include "filemetrics.h"
+#include "idquery.h"
 ;
 
-CREATE ELEMENT TABLE Fileid (
+CREATE STRUCT VIEW File (
 	path STRING FROM get_path(),
 	id INTEGER FROM get_id(),
 	fname STRING FROM get_fname(),
@@ -41,5 +42,68 @@ CREATE ELEMENT TABLE Fileid (
 	readonly INTEGER FROM get_readonly()
 );
 
-CREATE TABLE cscout.Fileids WITH BASE = files
-AS SELECT * FROM vector<Fileid>;
+CREATE VIRTUAL TABLE cscout.File
+USING STRUCT VIEW File
+WITH REGISTERED C NAME files
+WITH REGISTERED C TYPE vector<Fileid>;
+
+CREATE STRUCT VIEW Eclass (
+	len INTEGER FROM get_len(),
+	size INTEGER FROM get_size(),
+	unused BOOLEAN FROM is_unused(),					// True if this equivalence class is unintentionally unused
+	is_declared_unused BOOLEAN FROM get_attribute(is_declared_unused),	// Declared with __unused__ attribute
+	is_macro_token BOOLEAN FROM get_attribute(is_macro_token),		// Identifier stored in a macro
+
+	is_readonly BOOLEAN FROM get_attribute(is_readonly),			// Read-only; true if any member
+										// comes from an ro file
+	// The four C namespaces
+	is_suetag BOOLEAN FROM get_attribute(is_suetag),			// Struct/union/enum tag
+	is_sumember BOOLEAN FROM get_attribute(is_sumember),			// Struct/union member
+	is_label BOOLEAN FROM get_attribute(is_label),				// Goto label
+	is_ordinary BOOLEAN FROM get_attribute(is_ordinary),			// Ordinary identifier
+
+	is_macro BOOLEAN FROM get_attribute(is_macro),				// Name of an object or function-like macro
+	is_undefined_macro BOOLEAN FROM get_attribute(is_undefined_macro),	// Macro (heuristic: ifdef, defined)
+	is_macro_arg BOOLEAN FROM get_attribute(is_macro_arg),			// Macro argument
+	// The following are valid if is_ordinary is true:
+	is_cscope BOOLEAN FROM get_attribute(is_cscope),			// Compilation-unit (file) scoped
+				// identifier  (static)
+	is_lscope BOOLEAN FROM get_attribute(is_lscope),			// Linkage-unit scoped identifier
+	is_typedef BOOLEAN FROM get_attribute(is_typedef),			// Typedef
+	is_enum BOOLEAN FROM get_attribute(is_enum),				// Enumeration member
+	is_yacc BOOLEAN FROM get_attribute(is_yacc),				// Yacc identifier
+	is_function BOOLEAN FROM get_attribute(is_function),			// Function
+        FOREIGN KEY(members) FROM get_members() REFERENCES Tokens
+);
+
+CREATE VIRTUAL TABLE cscout.Eclass
+USING STRUCT VIEW Eclass
+WITH REGISTERED C TYPE Eclass;
+
+CREATE STRUCT VIEW Identifier (
+	id STRING FROM get_id(),
+	xfile BOOLEAN FROM get_xfile()
+);
+
+CREATE VIRTUAL TABLE cscout.Identifier
+USING STRUCT VIEW Identifier
+WITH REGISTERED C TYPE Identifier;
+
+CREATE STRUCT VIEW IdentifierProperties (
+      FOREIGN KEY(eclass) FROM first REFERENCES Eclass POINTER,
+      FOREIGN KEY(id) FROM second REFERENCES Identifier
+);
+
+CREATE VIRTUAL TABLE cscout.IdentifierProperties
+USING STRUCT VIEW IdentifierProperties
+WITH REGISTERED C NAME ids
+WITH REGISTERED C TYPE map<Eclass *, Identifier>;
+
+CREATE STRUCT VIEW Token (
+	fileid INTEGER FROM get_fileid().get_id(),
+	offset INTEGER FROM get_streampos()
+);
+
+CREATE VIRTUAL TABLE cscout.Tokens
+USING STRUCT VIEW Token
+WITH REGISTERED C TYPE set<Tokid>;
