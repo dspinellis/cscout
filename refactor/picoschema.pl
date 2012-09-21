8ee2e47ef433aaa6e0f0c2e6870706d4d1455aa1
Diomidis Spinellis <dds@aueb.gr>
2012-09-21 07:56:58 +0000

    Skip internal-only members

--- refactor/picoschema.pl
+++ refactor/picoschema.pl
@@ -5,19 +5,20 @@
 #
 
 while (<>) {
-	if (/^(\s*)#schema (\S+)\s+(\w+)\s+(.*)/) {
+	if (/^(\s*)#schema (\S+)\s+(\w+)\s+(\w+)\s+(.*)/) {
 		my $indent = $1;
 		my $file = $2;
 		my $schema = $3;
-		my $expr = $4;
+		my $type = $4;
+		my $expr = $5;
 		open(IN, $file) || die "Unable to open $file: $!\n";
 		while (<IN>) {
 			if (/BEGIN AUTOSCHEMA $schema$/ .. /END AUTOSCHEMA $schema$/) {
 				if (/\{\s*(\w+)\,\s*\"(\w+)\"\,\s*\"([^"]+)\"/) {
 					my $fieldname = $1;
 					my $name = $2;
+					next if ($name eq 'INTERNAL');
 					my $description = $3;
-					$type = (/\/\/ REAL$/ ? 'REAL' : 'INTEGER');
 					my $e = $expr;
 					$e =~ s/FIELDNAME/$fieldname/g;
 					print "$indent$name $type FROM $e,\n";
