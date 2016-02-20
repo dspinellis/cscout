9e51883df0f8e920f7514380362b33070fe6542c
Diomidis Spinellis <dds@aueb.gr>
2016-02-20 09:44:16 +0200

    Document how to run specific test cases

--- src/runtest.sh
+++ src/runtest.sh
@@ -18,7 +18,19 @@
 # along with CScout.  If not, see <http://www.gnu.org/licenses/>.
 #
 #
+# To run only a series of tests pass one of the following options
+# -TEST_AWK
+# -TEST_RECONST
+# -TEST_CPP
+# -TEST_C
+# -TEST_OBFUSCATION
 #
+# To run a single test set the corresponding environment variable e.g.
+# CFILES=c36-endlabel.c ./runtest.sh -TEST_C
+# RCFILES=c36-endlabel.c ./runtest.sh -TEST_RECONST
+# CPPFILES=cpp63-rescan.c ./runtest.sh -TEST_CPP
+#
+
 
 echo 'TAP version 13'
 
