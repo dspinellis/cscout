80b21d5b13304d09587642d563dad080fdb3b1aa
Diomidis Spinellis <dds@aueb.gr>
2024-08-02 08:29:47 +0300

    Fix documentation glitches

--- src/csmake.pl
+++ src/csmake.pl
@@ -92,13 +92,13 @@ if (defined $options{d}) {
 if (defined $options{h}) {
 print <<HELP;
 usage: csmake [ [-A] [-d] [-k] [-s cs_files_directory] [-T temporary_directory ] [-N rules_file] [-h] -- ] [make(1) options]
-    -d                      Run debug mode (it also keeps spy directory in place).
-    -h                      Prints help message.
+    -d                      Run in debug mode (it also keeps spy directory in place).
+    -h                      Print help message.
     -k                      Keep temporary directory in place.
     -s cs_files_directory   Create a separate CScout .cs file for each real executable.
     -A                      Generate cs projects for static libraries.
     -N rules_file           Run on an existing rules file.
-    -T temporary_directory  Set tempory directory.
+    -T temporary_directory  Set temporary directory.
 HELP
 exit();
 }
