994de1a1d598462b5db4dad8e13b52cb4052c724
Diomidis Spinellis <dds@aueb.gr>
2024-08-22 00:13:26 +0300

    Ensure new eids are applied to all records

--- src/dbmerge/ids.sql
+++ src/dbmerge/ids.sql
@@ -1,9 +1,31 @@
--- Add to ids missing records, and update any existing ones
--- with the merged value of the existing and the new ones.
+-- Create global tokens table with suitably mapped existing entries
+-- containing the merged value of the existing and the new ones.
+-- The eid_to_global_map table remaps all ECs, so all records have to be
+-- created anew.
 
-INSERT OR REPLACE INTO ids
+CREATE TABLE new_ids(
+  eid BIGINT PRIMARY KEY, -- Unique identifier key
+  name CHARACTER VARYING, -- Identifier name
+  readonly BOOLEAN, -- True if it appears in at least one read-only file
+  undefmacro BOOLEAN, -- True if it is apparantly an undefined macro
+  macro BOOLEAN, -- True if it a preprocessor macro
+  macroarg BOOLEAN, -- True if it a preprocessor macro argument
+  ordinary BOOLEAN, -- True if it is an ordinary identifier (variable or function)
+  suetag BOOLEAN, -- True if it is a structure, union, or enumeration tag
+  sumember BOOLEAN, -- True if it is a structure or union member
+  label BOOLEAN, -- True if it is a label
+  typedef BOOLEAN, -- True if it is a typedef
+  enum BOOLEAN, -- True if it is an enumeration member
+  yacc BOOLEAN, -- True if it is a yacc identifier
+  fun BOOLEAN, -- True if it is a function name
+  cscope BOOLEAN, -- True if its scope is a compilation unit
+  lscope BOOLEAN, -- True if it has linkage scope
+  unused BOOLEAN -- True if it is not used
+);
+
+INSERT INTO new_ids
   SELECT
-    egm.global_eid AS eid,
+    Coalesce(egma.global_eid, egmb.global_eid) AS eid,
     Coalesce(fma.name, fmb.name) AS name,
     Coalesce(fma.readonly, fmb.readonly) OR Coalesce(fmb.readonly, fma.readonly) AS readonly,
     Coalesce(fma.undefmacro, fmb.undefmacro) AND Coalesce(fmb.undefmacro, fma.undefmacro) AS undefmacro,
@@ -22,5 +44,13 @@ INSERT OR REPLACE INTO ids
     Coalesce(fma.unused, fmb.unused) AND Coalesce(fmb.unused, fma.unused) AS unused
   FROM
     adb.ids AS fma
-    LEFT JOIN eid_to_global_map AS egm ON egm.dbid = 5 AND egm.eid = fma.eid
-    LEFT JOIN ids AS fmb ON fmb.eid = egm.global_eid;
+    LEFT JOIN eid_to_global_map AS egma
+      ON egma.dbid = 5 AND egma.eid = fma.eid
+    -- Requires SQLite >= 3.39.0
+    FULL OUTER JOIN ids AS fmb
+      ON fma.eid = fmb.eid
+    LEFT JOIN eid_to_global_map AS egmb
+      ON egmb.dbid != 5 AND egmb.eid = fmb.eid;
+
+DROP TABLE IF EXISTS ids;
+ALTER TABLE new_ids RENAME TO ids;
--- src/dbmerge/tokens.sql
+++ src/dbmerge/tokens.sql
@@ -1,18 +1,42 @@
--- Insert into the global tokens table suitably mapped new entries
+-- Create global tokens table with suitably mapped existing entries
+-- The eid_to_global_map table remaps all ECs, so all records have to be
+-- created anew.
 
 CREATE INDEX IF NOT EXISTS adb.idx_tokens_eid ON tokens(eid);
 CREATE INDEX IF NOT EXISTS adb.idx_tokens_foffset ON tokens(foffset);
 
-INSERT INTO tokens
-  SELECT 
-    fid_map.global_fid AS fid,
-    atokens.foffset AS foffset,
-    eid_map.global_eid AS eid
+CREATE TABLE new_tokens(
+  fid INTEGER, -- File key
+  foffset INTEGER, -- Offset within the file
+  eid BIGINT, -- Identifier key
+  PRIMARY KEY(fid, foffset),
+  FOREIGN KEY(fid) REFERENCES files(fid),
+  FOREIGN KEY(eid) REFERENCES ids(eid)
+);
+
+INSERT INTO new_tokens
+  SELECT
+      fid_map.global_fid AS fid,
+      atokens.foffset,
+      eid_map.global_eid AS eid
     FROM adb.tokens AS atokens
     LEFT JOIN fileid_to_global_map as fid_map
       ON fid_map.dbid = 5 AND fid_map.fid = atokens.fid
     LEFT JOIN eid_to_global_map AS eid_map
       ON eid_map.dbid = 5 AND eid_map.eid = atokens.eid
-    LEFT JOIN tokens ON tokens.fid = fid_map.global_fid
-      AND tokens.foffset = atokens.foffset
-    WHERE tokens.eid IS NULL;
+  UNION
+  SELECT
+      tokens.fid,
+      tokens.foffset,
+      eid_map.global_eid AS eid
+    FROM tokens
+    LEFT JOIN fileid_to_global_map as fid_map
+      ON fid_map.global_fid = tokens.fid AND fid_map.dbid != 5
+    LEFT JOIN eid_to_global_map AS eid_map
+      ON eid_map.dbid = fid_map.dbid AND eid_map.eid = tokens.eid;
+
+DROP TABLE IF EXISTS tokens;
+ALTER TABLE new_tokens RENAME TO tokens;
+
+CREATE INDEX IF NOT EXISTS idx_tokens_eid ON tokens(eid);
+CREATE INDEX IF NOT EXISTS idx_tokens_foffset ON tokens(foffset);
