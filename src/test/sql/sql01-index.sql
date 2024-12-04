11ce76ccc9868ceaf8202964402a67483e00e961
Diomidis Spinellis <dds@aueb.gr>
2024-12-04 10:42:11 +0200

    Add further indexes

--- src/test/sql/sql01-index.sql
+++ src/test/sql/sql01-index.sql
@@ -1,4 +1,15 @@
 -- Speed up processing
 
-CREATE INDEX IF NOT EXISTS tokens_eid_idx on tokens(eid);
-CREATE INDEX IF NOT EXISTS tokens_fid_idx on tokens(fid);
+CREATE INDEX IF NOT EXISTS idx_tokens_eid ON tokens(eid);
+CREATE INDEX IF NOT EXISTS idx_tokens_fid_idx on tokens(fid);
+
+CREATE INDEX IF NOT EXISTS idx_fcalls_destid ON fcalls(destid);
+CREATE INDEX IF NOT EXISTS idx_fcalls_sourceid ON fcalls(sourceid);
+
+CREATE INDEX IF NOT EXISTS idx_functiondefs_functionid ON functiondefs(functionid);
+
+CREATE INDEX IF NOT EXISTS idx_ids_name ON ids(name);
+CREATE INDEX IF NOT EXISTS idx_ids_name ON ids(eid);
+
+CREATE INDEX IF NOT EXISTS idx_includers_cuid ON includers(cuid);
+CREATE INDEX IF NOT EXISTS idx_includers_includerid ON includers(includerid);
