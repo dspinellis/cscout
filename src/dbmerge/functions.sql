af7c44cd1900b47b83abae2b1cb6d0b0f0ad49d8
Diomidis Spinellis <dds@aueb.gr>
2025-10-21 23:12:23 +0300

    Fix functions merging
    The original query assumed that functionids remained stable for one
    of the tables, whereas they change for both.

--- src/dbmerge/functions.sql
+++ src/dbmerge/functions.sql
@@ -1,20 +1,50 @@
--- Insert or update into the global functions table suitably mapped new entries
+-- Create global functions table with suitably mapped entries
 
-INSERT OR REPLACE INTO functions
+CREATE TABLE new_functions(
+  ID BIGINT PRIMARY KEY, -- Unique function identifier
+  NAME CHARACTER VARYING, -- Function name (redundant; see FUNCTIONID)
+  ISMACRO BOOLEAN, -- True if a function-like macro (otherwise a C function)
+  DEFINED BOOLEAN, -- True if the function is defined within the workspace
+  DECLARED BOOLEAN, -- True if the function is declared within the workspace
+  FILESCOPED BOOLEAN, -- True if the function's scope is a single compilation unit (static or macro)
+  FID INTEGER, -- File key of the function's definition, declaration, or use
+  FOFFSET INTEGER, -- Offset of definition, declaration, or use within the file
+  FANIN INTEGER, -- Fan-in (number of callers)
+  FOREIGN KEY(FID) REFERENCES FILES(FID)
+);
+
+WITH fa AS (
   SELECT functionid_map.global_id AS id,
+      name, ismacro, defined, declared, filescoped,
+      fileid_map.global_fid AS fid, foffset, fanin
+    FROM adb.functions AS f
+    LEFT JOIN fileid_to_global_map AS fileid_map
+      ON fileid_map.dbid = 5 AND fileid_map.fid = f.fid
+    LEFT JOIN functionid_to_global_map AS functionid_map
+      ON functionid_map.dbid = 5 AND functionid_map.id = f.id
+),
+fb AS (
+  SELECT functionid_map.global_id AS id,
+      name, ismacro, defined, declared, filescoped,
+      fileid_map.global_fid AS fid, foffset, fanin
+    FROM functions AS f
+    LEFT JOIN fileid_to_global_map AS fileid_map
+      ON fileid_map.dbid != 5 AND fileid_map.global_fid = f.fid
+    LEFT JOIN functionid_to_global_map AS functionid_map
+      ON functionid_map.dbid != 5 AND functionid_map.id = f.id
+)
+INSERT INTO new_functions
+  SELECT
+      Coalesce(fa.id, fb.id) AS id,
       Coalesce(fa.name, fb.name) AS name,
       Coalesce(fa.ismacro, fb.ismacro) OR Coalesce(fb.ismacro, fa.ismacro) AS ismacro,
       Coalesce(fa.defined, fb.defined) OR Coalesce(fb.defined, fa.defined) AS defined,
       Coalesce(fa.declared, fb.declared) OR Coalesce(fb.declared, fa.declared) AS declared,
       Coalesce(fa.filescoped, fb.filescoped) OR Coalesce(fb.filescoped, fa.filescoped) AS filescoped,
-      fileid_map.global_fid AS fid,
+      Coalesce(fa.fid, fb.fid) AS fid,
       Coalesce(fa.foffset, fb.foffset) AS foffset,
       Max(Coalesce(fa.fanin, fb.fanin), Coalesce(fb.fanin, fa.fanin)) AS fanin
-    FROM adb.functions AS fa
-    LEFT JOIN fileid_to_global_map AS fileid_map
-      ON fileid_map.dbid = 5 AND fileid_map.fid = fa.fid
-    LEFT JOIN functionid_to_global_map AS functionid_map
-      ON functionid_map.dbid = 5
-        AND functionid_map.id = fa.id
-    LEFT JOIN functions AS fb
-      ON fb.id = functionid_map.global_id;
+    FROM fa FULL OUTER JOIN fb ON fa.id = fb.id;
+
+DROP TABLE functions;
+ALTER TABLE new_functions RENAME TO functions;
