6f671f736ed795a261dfc08c822416d632e04d1c
ujjwx1 <uaggarwal1_be23@thapar.edu>
2026-06-06 16:57:50 +0530

    Move general utilities out of cscout.cpp
    split_by_delimiter and version_info belong to neither the
    analysis engine nor the HTML interface. Moving them to a
    sepertate file gives these shared utilities a proper home
    and makes cscout.cpp smaller and more focused.

---
+++ src/util.h
@@ -0,0 +1,33 @@
+/*
+ * (C) Copyright 2008-2026 Diomidis Spinellis
+ *
+ * This file is part of CScout.
+ *
+ * CScout is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CScout is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CScout.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * General utility functions.
+ *
+ */
+
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> split_by_delimiter(std::string &s, char delim);
+std::string version_info(bool html);
+
+#endif  /* UTIL_H */
